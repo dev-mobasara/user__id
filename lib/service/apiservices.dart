c0b6411cd830f40b91652455a90c61b5bfe39704
dev-mobasara <mkmobasara.dev@gmail.com>
2022-08-23 16:19:11 +0600

    solve http package problem. and add API service according to model.

---
+++ lib/service/apiservices.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:users_id/model/model.dart';
+
+class APIService {
+  static Future<List<UserModel>> fetchUserModel() async {
+    final response = await http.get(
+      Uri.parse(
+          'https://metaspook.github.io/json_api/json/products_gadget.json'),
+    );
+
+    if (response.statusCode == 200) {
+      final user = [
+        for (final users in jsonDecode(response.body))
+          UserModel.fromJson(users as Map<String, dynamic>)
+      ];
+      return user;
+    } else {
+      throw Exception("Failed to load");
+    }
+  }
+}
