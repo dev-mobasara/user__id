3d0de9bd1f23e20e2c9d429e34e6fe5d7afd7a22
dev-mobasara <mkmobasara.dev@gmail.com>
2022-08-23 15:12:48 +0600

    create model from json file

---
+++ lib/model/model.dart
@@ -0,0 +1,23 @@
+class UserModel {
+  UserModel({
+    required this.postId,
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.body,
+  });
+
+  final int postId;
+  final int id;
+  final String name;
+  final String email;
+  final String body;
+
+  factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
+        postId: json["postId"],
+        id: json["id"],
+        name: json["name"],
+        email: json["email"],
+        body: json["body"],
+      );
+}
